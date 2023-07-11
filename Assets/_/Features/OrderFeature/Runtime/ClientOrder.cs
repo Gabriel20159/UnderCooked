9574b775a803d5790722f2802f5a46c4486ac870
Théo Constant <to.constant@gmail.com>
2023-07-11 13:33:09 +0200

    [OrderFeature] set initial Scripts up

---
+++ Assets/_/Features/OrderFeature/Runtime/ClientOrder.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OrderFeature.Runtime
+{
+    [CreateAssetMenu(fileName = "ClientOrder",menuName = "ClientOrder")]
+    public class ClientOrder : ScriptableObject
+    {
+        #region Public Members
+
+        // public Ingredients[] _possibleIngredientsList;
+        // public Ingredients[] _recipe;
+        // public float _timeRemaining;
+        //
+        // #endregion
+        //
+        //
+        // #region Unity API
+        //
+        // private void OnEnable()
+        // {
+        //     _recipe = new Ingredients[2];
+        //     
+        // }
+
+        #endregion
+    }
+}
---
+++ Assets/_/Features/OrderFeature/Runtime/OrderManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OrderFeature.Runtime
+{
+    public class OrderManager : MonoBehaviour
+    {
+        #region Public Members
+        #endregion
+
+
+        #region Unity API
+        #endregion
+
+
+        #region Private and Protected
+        #endregion
+    }
+}
