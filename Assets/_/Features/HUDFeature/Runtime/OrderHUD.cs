e7d82592a6c7ebcbb93de5d7b14630c17a158c2b
Gattesco Dorian <doriangattesco@gmail.com>
2023-07-11 14:21:13 +0200

    [HUD] Add orderHUD asset

--- Assets/_/Features/HUDFeature/Runtime/OrderHUD.cs
+++ Assets/_/Features/HUDFeature/Runtime/OrderHUD.cs
@@ -1,23 +1,24 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HUDFeature.Runtime
 {
-    public class PauseHUD : MonoBehaviour
+    public class OrderHUD : MonoBehaviour
     {
         #region Public Members
+
+        public Image m_orderImage;
+        public Image m_orderRecipe;
+        public Slider m_timeSlider;
 
         #endregion Public Members
 
         #region Unity API
-
+        
         #endregion Unity API
 
         #region Main Methods
-
-        public void OnPauseEventHandler()
-        {
-            _PauseMenu.SetActive(!_PauseMenu.activeSelf);
-        }
 
         #endregion Main Methods
 
@@ -26,9 +27,6 @@ namespace HUDFeature.Runtime
         #endregion Utils
 
         #region Private and Protected Members
-
-        [SerializeField] private GameObject _PauseMenu;
-
         #endregion Private and Protected Members
     }
 }
---
+++ Assets/_/Features/HUDFeature/Runtime/OrderSpawnerHUD.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace HUDFeature.Runtime
+{
+    public class OrderSpawnerHUD : MonoBehaviour
+    {
+        #region Public Members
+
+        #endregion Public Members
+
+        #region Unity API
+
+        #endregion Unity API
+
+        #region Main Methods
+
+        public void OnNewOrderEventHandler(Sprite orderImage, Sprite orderRecipe)
+        {
+            GameObject order = Instantiate(_orderPrefab, _orderCanvas.transform, true);
+            var orderScript = order.GetComponent<OrderHUD>();
+            orderScript.m_orderImage.sprite = orderImage;
+            orderScript.m_orderRecipe.sprite = orderRecipe;
+        }
+
+        #endregion Main Methods
+
+        #region Utils
+
+        #endregion Utils
+
+        #region Private and Protected Members
+
+        [SerializeField] private GameObject _orderPrefab;
+        [SerializeField] private GameObject _orderCanvas;
+
+        #endregion Private and Protected Members
+    }
+}
