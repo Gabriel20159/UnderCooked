d2e91459b726494a84a14598f8be1c799b6f2736
Gattesco Dorian <doriangattesco@yahoo.fr>
2023-07-12 12:27:44 +0200

    [Stove] Add Stove Prefab

---
+++ Assets/_/Features/InteractableFeature/Runtime/Furnitures/Stove.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PickableFeature.Runtime;
+using UnityEngine;
+
+namespace InteractableFeature.Runtime
+{
+    public class Stove : Furniture
+    {
+   
+
+    	#region Public Members
+
+	
+
+    	#endregion
+
+
+    	#region Unity API
+
+    	#endregion
+
+
+    	#region Main Methods
+
+	    void Update()
+	    {
+		//   if (_currentPickable is Pickable pickable)
+		    {
+			    
+		    }
+	    }
+
+    	#endregion
+
+
+    	#region Utils
+
+    	#endregion
+
+
+    	#region Private and Protected Members
+
+    	#endregion
+
+
+    }
+}
